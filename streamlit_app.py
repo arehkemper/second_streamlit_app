3a16450f90b28fb843286f652f788b8c29819441
Adele Rehkemper <arehkemper@gmail.com>
2022-10-19 16:46:18 -0500

    Update streamlit_app.py
    selection tool

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,6 @@ my_cur=my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
+
+# Let's put a pick list here so they can pick the color
+option = streamlit.selectbox('Pick a sweatsuit color or style:', 'Pink')
