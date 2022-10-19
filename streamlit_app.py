e5e0956c8ab23648e710190afa6b5d7b209c5218
Adele Rehkemper <arehkemper@gmail.com>
2022-10-19 16:48:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 
 # Let's put a pick list here so they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:')
+option = streamlit.selectbox('Pick a sweatsuit color or style:',pink)
 
 # put the data into a dataframe
 df = pandas.DataFrame(my_catalog)
